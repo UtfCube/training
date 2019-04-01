0826a80493c1a3a6567d1d3c2389717872e2b8b9
UtfCube <black_fox_0@mail.ru>
2019-04-01 14:15:20 +0300

    add endpoint for user info

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Get, UseGuards, Headers } from '@nestjs/common';
+import { UserService } from './user.service';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from 'src/auth/auth.service';
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(
+        private readonly userService: UserService,
+        private readonly authService: AuthService
+        ) {}
+
+    @Get()
+    @UseGuards(AuthGuard('jwt'))
+    async getInfo(@Headers('authorization') token: string) {
+        const jwt = this.authService.decodeJwt(token.slice(7));
+        const user = await this.userService.findById(jwt.sub);
+        delete user.trainings;
+        return user;
+    }
+}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,11 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entity/user.entity';
+import { AuthService } from 'src/auth/auth.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService, AuthService]
 })
 export class UserModule {}
